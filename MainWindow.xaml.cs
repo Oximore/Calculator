1db2bd049f1770cbe91b8d3a3c2dce23a16d2810
Benjamin <benjamin.lux.travel+git@gmail.com>
2024-07-26 11:15:47 +0200

    Modify: Apply Sugested modification from Frederic

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,379 +1,72 @@
-﻿using System.Diagnostics;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Input;
 
 namespace Calculator
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private string operand1 = "", operand2 = "";
+        private string @operator = ""; // @ car operator mot-clef c#
+
         public MainWindow()
         {
             InitializeComponent();
-            labelOperation.Content = " ";
-            labelResult.Content = " ";
         }
 
-        private enum Operator
-        { Plus, Minus, Multiply, Divide }
-
-        private class Operation
+        private void DigitButton_Click(object sender, RoutedEventArgs e)
         {
-            public List<double> Numbers = new List<double>();
-            public List<Operator> Operators = new List<Operator>();
+            if (sender is not Button button)
+                return;
 
-            public bool Computable => (Numbers.Count == Operators.Count + 1);
-            public bool Computed = false;
+            if (button.Content is not string digit)
+                return;
 
-            public double Current = 0;
-            public double? Result = null;
+            if (!string.IsNullOrEmpty(@operator) &&
+                string.IsNullOrEmpty(operand2))
+                resultText.Text = "";
 
-
-            public void Clear()
-            {
-                Numbers.Clear(); Operators.Clear();
-                Computed = false;
-                Current = 0;
-                Result = null;
-            }
-
-            public void ValidateCurrent()
-            {
-                Numbers.Add(Current);
-                Current = 0;
-            }
-
-            public void AddToCurrent(int i)
-            {
-                if (Computed)
-                    Clear();
-
-                Current = Current * 10 + i;
-                //TODO comma
-            }
-
-            public void AddOperator(Operator op)
-            {
-                if (Computed)
-                    return; // error
-
-                ValidateCurrent();
-                Operators.Add(op);
-            }
-
-            public double Compute()
-            {
-                if (Computed && Result != null)
-                    return (double) Result;
-
-                ValidateCurrent();
-
-                double result = 0;
-                if (Computable)
-                {
-                    result = Numbers.ElementAt(0);
-                    for (int i = 0; i < Numbers.Count-1; i++)
-                    {
-                        double value = Numbers[i+1];
-                        
-                        switch (Operators[i])
-                        {
-                            case Operator.Plus:
-                                result = result + value;
-                                break;
-                            case Operator.Minus:
-                                result = result - value;
-                                break;
-                            case Operator.Multiply:
-                                result = result * value;
-                                break;
-                            case Operator.Divide:
-                                result = result / value;
-                                break;
-                            default:
-                                break;
-                        }
-                    }
-
-                    Result = result;
-                    Computed = true;
-                }
-                else
-                {
-                    Debug.WriteLine($" *********** ERROR in file *******");
-                }
-                return result;
-            }
-
-            public override string ToString()
-            {
-                string sResult = "";
-                for (int i = 0; i < Numbers.Count; i++)
-                {
-                    sResult += " " + Numbers[i];
-                    if (Operators.Count > i)
-                    {
-                        var op = Operators[i] switch
-                        {
-                            Operator.Plus => '+',
-                            Operator.Minus => '-',
-                            Operator.Multiply => '*',
-                            Operator.Divide => '/',
-                            _ => '?',
-                        };
-                        sResult += $" {op}";
-                    }
-                }
-
-                if (Computed)
-                {
-                    sResult += $" = {Result}";
-                }
-                return sResult;
-            }
-
-            internal void AddComma()
-            {
-                throw new NotImplementedException();
-            }
-        }
-
-        private Operation CurrentOp = new Operation();
-
-
-        private void RefreshCalc(Operation op)
-        {
-            labelOperation.Content = CurrentOp.ToString();
-            if (CurrentOp.Computed)
-                labelResult.Content = CurrentOp.Result;
+            if (string.IsNullOrEmpty(@operator))
+                operand1 += digit;
             else
-                labelResult.Content = CurrentOp.Current;
+                operand2 += digit;
+
+            resultText.Text += digit;
         }
 
-
-        private void DisplayOperation()
+        private void OperatorButton_Click(object sender, RoutedEventArgs e)
         {
-            Debug.WriteLine(CurrentOp.ToString() + "*******");
-            labelOperation.Content = CurrentOp.ToString();
+            if (sender is not Button button)
+                return;
+
+            if (button.Content is not string @operator)
+                return;
+
+            this.@operator = @operator;
         }
 
-        private void DisplayResult()
+        private void EqualButton_Click(object sender, RoutedEventArgs e)
         {
-            if (CurrentOp.Computed)
-                labelResult.Content = CurrentOp.Result;
-        }
+            if (!double.TryParse(operand1, out double value1))
+                return;
 
-        private void DebugButton(int index)
-        {
-            Debug.WriteLine($" --- Boutton {index} clicked");
-            Debug.WriteLine($" --- curent op: {CurrentOp.ToString()}");
-            Debug.WriteLine($" --- curent nb: {CurrentOp.Current}");
+            if (!double.TryParse(operand2, out double value2))
+                return;
 
-        }
+            double result;
 
-        private void DebugButton(string msg)
-        {
-            Debug.WriteLine($" --- Boutton {msg} clicked");
-            Debug.WriteLine($" --- curent op: {CurrentOp.ToString()}");
-            Debug.WriteLine($" --- curent nb: {CurrentOp.Current}");
-        }
-
-        private void Click_0(object sender, RoutedEventArgs e)
-        {
-            TypeNumber(0);
-        }
-
-        private void Click_1(object sender, RoutedEventArgs e)
-        {
-            TypeNumber(1);
-        }
-
-        private void Click_2(object sender, RoutedEventArgs e)
-        {
-            TypeNumber(2);
-        }
-
-        private void Click_3(object sender, RoutedEventArgs e)
-        {
-            TypeNumber(3);
-        }
-
-        private void Click_4(object sender, RoutedEventArgs e)
-        {
-            TypeNumber(4);
-        }
-
-        private void Click_5(object sender, RoutedEventArgs e)
-        {
-            TypeNumber(5);
-        }
-
-        private void Click_6(object sender, RoutedEventArgs e)
-        {
-            TypeNumber(6);
-        }
-
-        private void Click_7(object sender, RoutedEventArgs e)
-        {
-            TypeNumber(7);
-        }
-
-        private void Click_8(object sender, RoutedEventArgs e)
-        {
-            TypeNumber(8);
-        }
-
-        private void Click_9(object sender, RoutedEventArgs e)
-        {
-            TypeNumber(9);
-        }
-
-        private void Click_Plus(object sender, RoutedEventArgs e)
-        {
-            TypePlus();
-        }
-
-        private void Click_Minus(object sender, RoutedEventArgs e)
-        {
-            TypeMinus();
-        }
-
-        private void Click_Multiply(object sender, RoutedEventArgs e)
-        {
-            TypeMultiply();
-        }
-
-        private void Click_Divide(object sender, RoutedEventArgs e)
-        {
-            TypeDivide();
-        }
-
-        private void Click_Equal(object sender, RoutedEventArgs e)
-        {
-            TypeEqual();
-        }
-
-        private void Click_Comma(object sender, RoutedEventArgs e)
-        {
-            TypeComma();
-        }
-
-
-        private void OnKeyDownHandler(object sender, KeyEventArgs e)
-        {
-            switch (e.Key)
+            switch (@operator)
             {
-                case Key.NumPad0:
-                    TypeNumber(0);
-                    break;
-                case Key.NumPad1:
-                    TypeNumber(1);
-                    break;
-                case Key.NumPad2:
-                    TypeNumber(2);
-                    break;
-                case Key.NumPad3:
-                    TypeNumber(3);
-                    break;
-                case Key.NumPad4:
-                    TypeNumber(4);
-                    break;
-                case Key.NumPad5:
-                    TypeNumber(5);
-                    break;
-                case Key.NumPad6:
-                    TypeNumber(6);
-                    break;
-                case Key.NumPad7:
-                    TypeNumber(7);
-                    break;
-                case Key.NumPad8:
-                    TypeNumber(8);
-                    break;
-                case Key.NumPad9:
-                    TypeNumber(9);
-                    break;
-                case Key.Enter:
-                    TypeEqual();
-                    break;
-
-                case Key.Add:
-                    TypePlus();
-                    break;
-                case Key.Subtract:
-                    TypeMinus();
-                    break;
-                case Key.Multiply:
-                    TypeMultiply();
-                    break;
-                case Key.Divide:
-                    TypeDivide();
-                    break;
-                case Key.OemComma: // TODO check
-                    TypeComma();
-                    break;
-                default:
-                    break;
-            }
-        }
-
-
-        private void TypeNumber(int i)
-        {
-            DebugButton(i);
-            if (CurrentOp.Computed)
-            {
-                CurrentOp.Clear();
+                case "+": result = value1 + value2; break;
+                case "−": result = value1 - value2; break;
+                case "×": result = value1 * value2; break;
+                case "÷": result = value1 / value2; break;
+                default: return;
             }
 
-            CurrentOp.AddToCurrent(i);
-            RefreshCalc(CurrentOp);
-        }
+            resultText.Text = result.ToString("0.#########");
 
-        private void TypePlus()
-        {
-            DebugButton("plus");
-            CurrentOp.AddOperator(Operator.Plus);
-            RefreshCalc(CurrentOp);
-        }
-
-        private void TypeMinus()
-        {
-            DebugButton("minus");
-            CurrentOp.AddOperator(Operator.Minus);
-            RefreshCalc(CurrentOp);
-        }
-
-        private void TypeMultiply()
-        {
-            DebugButton("multiply");
-            CurrentOp.AddOperator(Operator.Multiply);
-            RefreshCalc(CurrentOp);
-        }
-
-        private void TypeDivide()
-        {
-            DebugButton("divide");
-            CurrentOp.AddOperator(Operator.Divide);
-            RefreshCalc(CurrentOp);
-        }
-
-        private void TypeComma()
-        {
-            DebugButton("comma");
-            CurrentOp.AddComma();
-            RefreshCalc(CurrentOp);
-        }
-
-        private void TypeEqual()
-        {
-            DebugButton("equal");
-            CurrentOp.Compute();
-            //double result = CurrentOp.Result;
-            RefreshCalc(CurrentOp);
+            // Reset
+            operand1 = operand2 = @operator = "";
         }
     }
 }
